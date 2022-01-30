4eef74efe0586363797dcb58e12709fa56e424d0
Paul Applegate <paul@theapplegates.com>
2022-01-30 15:03:59 +0000

    aa

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,28 +21,6 @@ module.exports = {
           maxWidth: 800,
           wrapperStyle: fluidResult => `flex:${_.round(fluidResult.aspectRatio, 2)};`,
         },
-      }
-      resolve: '@kentico/gatsby-source-kontent',
-      options: {
-        projectId: process.env.KONTENT_PROJECT_ID, // Fill in your Project ID
-        // if false used authorization key for secured API
-        usePreviewUrl: !!(
-          process.env.KONTENT_PREVIEW_ENABLED &&
-          process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true'
-        ),
-        authorizationKey:
-          process.env.KONTENT_PREVIEW_ENABLED &&
-          process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true'
-            ? process.env.KONTENT_PREVIEW_KEY
-            : undefined,
-        languageCodenames: process.env.KONTENT_LANGUAGE_CODENAMES.split(
-          ','
-        ).map((lang) => lang.trim()),
-        experimental: {
-          managementApiTriggersUpdate: true,
-        },
-      },
-    },
     {
       resolve: 'kontent-used-by-content-items', // local plugin
       options: {
